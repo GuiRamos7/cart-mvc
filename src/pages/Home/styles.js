e88dc9ee3a5c46f046e9dcc15e86f25fba2722bb
Guilherme Ramos <guilherme.ramos@matchboxbrasil.com>
2022-08-25 01:08:43 -0300

    feat: creating mobile version of home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,9 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   .products {
-    margin: 35px 0;
+    margin: 35px auto;
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 50px 0;
+    grid-template-columns: repeat(auto-fill, 300px);
+    gap: 50px auto;
+
+    @media (max-width: 500px) {
+      justify-items: center;
+      align-items: center;
+    }
   }
 `;
